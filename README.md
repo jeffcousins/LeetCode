d023493f3b9fe0ecc8441c891ab1dc723ac326fa
Jeff Cousins <jeffcousins@me.com>
2016-07-21 18:28:32 -0500

    Add roman to integer solution

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ My solutions to LeetCode Problems
 |---| ----- | --------- | ---------- |
 |1|[Two Sum](https://leetcode.com/problems/two-sum/) | [JavaScript](./algorithms/two-sum.js) |Easy|
 |7|[Reverse Integer](https://leetcode.com/problems/reverse-integer/) | [JavaScript](./algorithms/reverse-integer.js) |Easy|
+|13|[Roman to Integer](https://leetcode.com/problems/roman-to-integer/) | [JavaScript](./algorithms/roman-to-integer.js) |Easy|
---
+++ algorithms/roman-to-integer.js
@@ -0,0 +1,40 @@
+/**
+ * 13. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/
+ *
+ * Given a roman numeral, convert it to an integer.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  var digits = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+
+  var total = 0;
+  var curr;
+  var next;
+
+  for (i = 0; i < s.length; i++) {
+    curr = digits[s[i]];
+    next = digits[s[i + 1]];
+
+    if (next && curr < next) {
+      total -= curr;
+    } else {
+      total += curr;
+    }
+  }
+
+  return total;
+};
