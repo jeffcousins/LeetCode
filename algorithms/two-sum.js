f6b53eb33fad3c16cd44c96f00c75f7c7035642b
Jeff Cousins <jeffcousins@me.com>
2016-07-30 14:10:55 -0500

    Add URL

--- algorithms/two-sum.js
+++ algorithms/two-sum.js
@@ -1,5 +1,6 @@
 /**
  * 1. Two Sum
+ * https://leetcode.com/problems/two-sum/
  *
  * Given an array of integers, return indices of the two numbers such that
  * they add up to a specific target. You may assume that each input would
