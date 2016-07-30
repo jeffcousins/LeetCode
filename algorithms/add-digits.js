27b3f6bb4c9a213a02e30dacba7897c50a78930e
Jeff Cousins <jeffcousins@me.com>
2016-07-30 14:48:14 -0500

    Refactor

--- algorithms/add-digits.js
+++ algorithms/add-digits.js
@@ -3,6 +3,7 @@
  * https://leetcode.com/problems/add-digits/
  *
  * Given a non-negative integer num, repeatedly add all its digits until the
+ * result has only one digit.
  *
  * For example:
  * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2.
@@ -14,15 +15,15 @@
  * @return {number}
  */
 var addDigits = function(num) {
-  var digits;
+  var newSum;
 
   if (num < 10) {
     return num;
   }
 
-  digits = num.toString().split('');
-
-  return addDigits(digits.reduce(function(sum, digit) {
+  newSum = num.toString().split('').reduce(function(sum, digit) {
     return sum + Number(digit);
-  }, 0));
+  }, 0);
+
+  return addDigits(newSum);
 };
